0581e79de834eb2aa6e021fdc3684e4b82b1e8e2
Oliversoder06 <lexrudolf06@gmail.com>
2024-11-16 22:34:05 +0100

    Pleasseee wooorrrkkk

--- lib/db.ts
+++ lib/db.ts
@@ -6,10 +6,10 @@ let connection: mysql.Connection;
 export async function getConnection() {
     if (!connection) {
         connection = await mysql.createConnection({
-            host: 'localhost',
-            user: 'your_username',
-            password: 'ben10123',
-            database: 'mysql_practice',
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE,
         });
     }
     return connection;
